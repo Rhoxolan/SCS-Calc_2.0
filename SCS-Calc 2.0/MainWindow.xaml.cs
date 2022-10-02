666c7ae46375ee234fcf5e712277f1a341df10c2
Rhoxolan <pba17mail@gmail.com>
2022-10-03 00:32:12 +0300

    Add Page

--- SCS-Calc 2.0/MainWindow.xaml.cs
+++ SCS-Calc 2.0/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace SCS_Calc_2._0
 {
@@ -20,9 +7,17 @@ namespace SCS_Calc_2._0
     /// </summary>
     public partial class MainWindow : Window
     {
+        private CalculatePage calculatePage;
+
         public MainWindow()
         {
             InitializeComponent();
+            calculatePage = new();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            pageFrame.Content = calculatePage;
         }
     }
 }
