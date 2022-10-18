175cc4d4d7a527defb256b9a64b17923ea33fd07
Rhoxolan <pba17mail@gmail.com>
2022-10-18 12:49:02 +0300

    Add

--- SCS-Calc 2.0/CalculatePage/CalculatePageViewModel.cs
+++ SCS-Calc 2.0/CalculatePage/CalculatePageViewModel.cs
@@ -1,12 +1,34 @@
-﻿namespace SCS_Calc_2._0
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+
+namespace SCS_Calc_2._0
 {
-    public class CalculatePageViewModel
+    [INotifyPropertyChanged]
+    public partial class CalculatePageViewModel
     {
         private readonly ApplicationModel model;
 
         public CalculatePageViewModel(ApplicationModel model)
         {
             this.model = model;
+        }
+
+        public double MinPermanentLink { get; set; } = 1;
+
+        public double MaxPermanentLink { get; set; } = 1;
+
+        public int NumberOfWorkplaces { get; set; } = 1;
+
+        public int NumberOfPorts { get; set; } = 1;
+
+        public double? CableHankMeterage { get; set; } = 305;
+
+        [RelayCommand]
+        private void SetCableHankMeterage(string value)
+        {
+            CableHankMeterage = Convert.ToDouble(value);
+            OnPropertyChanged(nameof(CableHankMeterage));
         }
     }
 }
---
+++ SCS-Calc 2.0/Converters/IntegerToBooleanConverter.cs
@@ -0,0 +1,6 @@
+﻿namespace SCS_Calc_2._0.Converters
+{
+    public class IntegerToBooleanConverter
+    {
+    }
+}
