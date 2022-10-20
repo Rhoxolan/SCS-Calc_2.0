3fd0a9e9de5a0a3ebc24cf8721c6a3873b5300f7
Rhoxolan <pba17mail@gmail.com>
2022-10-20 11:10:45 +0300

    Add

--- SCS-Calc 2.0/AdvancedParametersPage/AdvancedParametersPageViewModel.cs
+++ SCS-Calc 2.0/AdvancedParametersPage/AdvancedParametersPageViewModel.cs
@@ -1,12 +1,40 @@
-﻿namespace SCS_Calc_2._0
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SCS_Calc_2._0
 {
-    public class AdvancedParametersPageViewModel
+    [INotifyPropertyChanged]
+    public partial class AdvancedParametersPageViewModel
     {
         private readonly ApplicationModel model;
 
         public AdvancedParametersPageViewModel(ApplicationModel model)
         {
             this.model = model;
+            model.TechnologicalReserveChanged += TechnologicalReserveChanged;
+            model.DiapasonsChanged += DiapasonsChanged;
+        }
+
+        public double TechnologicalReserve
+        {
+            get => model.TechnologicalReserve;
+            set => model.TechnologicalReserve = value;
+        }
+
+        public decimal TechnologicalReserveDiapasonMin => model.Diapasons.TechnologicalReserveDiapason.Min;
+
+        public decimal TechnologicalReserveDiapasonMax => model.Diapasons.TechnologicalReserveDiapason.Max;
+
+        //Обработчик для изменения значения коэффициента технологического запаса
+        private void TechnologicalReserveChanged(object? sender = null, object? args = null)
+        {
+            OnPropertyChanged(nameof(TechnologicalReserve));
+        }
+
+        //Обработчик для изменения значения даипазонов вводимых параметров расчёта конфигураций
+        private void DiapasonsChanged(object? sender = null, object? args = null)
+        {
+            OnPropertyChanged(nameof(TechnologicalReserveDiapasonMin));
+            OnPropertyChanged(nameof(TechnologicalReserveDiapasonMax));
         }
     }
 }
