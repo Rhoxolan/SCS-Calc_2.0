77e563c5dca68f84bde68e892a3e75e67f5027e3
Rhoxolan <pba17mail@gmail.com>
2022-10-27 13:45:10 +0300

    Add

---
+++ SCS-Calc 2.0/Converters/DateTimeToStringConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SCS_Calc_2._0.Converters
+{
+    public class DateTimeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime dateTime = (DateTime)value;
+            return $"{dateTime.ToShortDateString()} {dateTime.ToLongTimeString()}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+
+        public static DateTimeToStringConverter Instance { get; } = new();
+    }
+}
